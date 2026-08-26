8308586a65e64a65cc9d156f6d9a39d7e6577eb8
aebute <seanameh@gmail.com>
2026-08-26 12:42:09 +0100

    fix: update fetch URL to /api/v1/process-material

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         formData.append("text", inputMaterial);
       }
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/process`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1//process-material`, {
         method: "POST",
         body: formData,
       });
